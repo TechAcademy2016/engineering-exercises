37a8f97ffa2f6fe5c9283b0ea54db921d412c775
John McCloskey <john.mccloskey@uk.pwc.com>
2016-03-22 12:08:26 +0000

    Example usages

--- fizzbuzz-solution/src/main/java/FizzBuzzChallenge.java
+++ fizzbuzz-solution/src/main/java/FizzBuzzChallenge.java
@@ -1,6 +1,31 @@
 
 public class FizzBuzzChallenge {
     public static void main(String[] args) {
+        //printOneToOneHundred();
+
+        //printValueOf(1000);
+        //printValueOf(1001);
+        //printValueOf(1093836495);
+
+        printRange(1, 100);
+        printRange(105, 300);
+
+    }
+
+    private static void printRange(int x, int y) {
+        while( x <= y) {
+            System.out.println(FizzBuzz.getValue(x));
+            x++;
+        }
+    }
+
+    private static void printValueOf(int i) {
+        System.out.println(FizzBuzz.getValue(i));
+    }
+
+
+    //less reusable than print range
+    private static void printOneToOneHundred() {
         for (int i = 1; i <= 100; i++) {
             System.out.println(FizzBuzz.getValue(i));
         }
